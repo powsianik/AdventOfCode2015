512492705e3aa9aff9753c7a926ad613a38b4831
powsianik <Przemek.Owsianik@yahoo.com>
2016-11-27 19:52:15 +0100

    Day 1
    Added files for puzzle from Day 1.

---
+++ Day1/Day1.py
@@ -0,0 +1,52 @@
+''''
+Day1:
+Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+
+An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+
+The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+
+For example:
+
+(()) and ()() both result in floor 0.
+((( and (()(()( both result in floor 3.
+))((((( also results in floor 3.
+()) and ))( both result in floor -1 (the first basement level).
+))) and )())()) both result in floor -3.
+To what floor do the instructions take Santa?
+
+Your puzzle answer was 74.
+
+--- Part Two ---
+
+Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+) causes him to enter the basement at character position 1.
+()()) causes him to enter the basement at character position 5.
+What is the position of the character that causes Santa to first enter the basement?
+
+Your puzzle answer was 1795.
+'''
+inputInstructions = open("input.txt").read()
+floor = 0
+characterNum = 0;
+characterWhenSantaGoToBasement = 0
+isCheckAgainstBasement = True;
+
+for character in inputInstructions:
+    characterNum += 1
+
+    if (character == '('):
+        floor += 1
+    elif(character == ')'):
+        floor -= 1
+
+    if(isCheckAgainstBasement):
+        if(floor == -1):
+            characterWhenSantaGoToBasement = characterNum
+            isCheckAgainstBasement = False
+
+print(floor)
+print("Santo go to the basement for the first time when characterNum was " + str(characterWhenSantaGoToBasement))
